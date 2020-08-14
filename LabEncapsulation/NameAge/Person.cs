9368b4105517c8be9a379ca34a799d5417cde7a4
Yusmen <mojito@abv.bg>
2020-08-14 18:21:04 +0300

    Adding Encapsulation Lab Tasks

---
+++ LabEncapsulation/NameAge/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstname,string lastname,int age,decimal salary)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Age = age;
+            this.Salary = salary;
+
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (value < 460)
+                {
+                    
+                    throw new ArgumentException("Salary cannot be less than 460 leva");
+                    
+                }
+                salary = value;
+
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value == 0 || value < 0)
+                {
+                    throw new ArgumentException("Age " +
+                        "cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+          
+        }
+
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Lirst name cannot" +
+                        " contain fewer than 3 symbols");
+                }
+                this.lastName = value;
+            }
+           
+        }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot" +
+                        " contain fewer than 3 symbols");
+                }
+                this.firstName = value;
+            }
+           
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.salary = this.salary + this.salary * percentage / 100;
+
+            }
+            else
+            {
+                this.salary = this.salary + this.salary * percentage / 200;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName}  {this.LastName} receives {this.salary:f2} leva.";
+        }
+
+
+
+
+    }
+}
---
+++ LabEncapsulation/NameAge/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+
+{
+   public  class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people=new List<Person>();
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                Person person = new Person(input[0], input[1],
+                    int.Parse(input[2]),decimal.Parse(input[3]));
+                people.Add(person);
+
+            }
+            Team team = new Team("Softuni");
+            
+            foreach(var p in people)
+            {
+                team.AddPlayer(p);
+            }
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+            
+        }
+    }
+}
