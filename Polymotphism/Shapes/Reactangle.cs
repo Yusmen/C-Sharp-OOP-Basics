44cf1789586ef15eb5097066b257e95567a46966
Yusmen <mojito@abv.bg>
2020-08-16 20:32:33 +0300

    Adding Polymorphism Lab tasks

---
+++ Polymotphism/Shapes/Reactangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Reactangle : Shape
+    {
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Reactangle(double height,double width)
+        {
+            this.Width = width;
+            this.Height = height;
+                
+        }
+        
+
+
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}
---
+++ Polymotphism/Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Shape rectangle = new Reactangle(5, 6);
+            Shape circle = new Circle(10);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+           
+        }
+    }
+}
